3a95f926d7a490a4bc4d0af45d9503384789b898
Tim McGilchrist <timmcgil@gmail.com>
2019-10-02 08:13:14 +1000

    Add QuickCheck generator for queue names.

--- libraries/sqs/lib_test/aws_sqs_test.ml
+++ libraries/sqs/lib_test/aws_sqs_test.ml
@@ -1,6 +1,9 @@
 open OUnit
 open Aws_sqs
 
+(* TODO Maybe load this from ENV variable *)
+let test_region = "us-east-1"
+
 let from_opt = function
   | None -> assert false
   | Some(x) -> x
@@ -17,38 +20,50 @@ module TestSuite(Runtime : sig
     val un_m : 'a m -> 'a
   end) = struct
 
-  let create_delete_queue_test () =
-    let res = Runtime.(un_m (run_request
-                               ~region:"us-east-1"
-                               (module CreateQueue)
-                               (Types.CreateQueueRequest.make ~queue_name:"test_queue"()))) in
-    "Create Queue"
-    @? begin match res with
-       | `Ok resp ->
-          Printf.printf "%s\n" (Yojson.Basic.to_string (Types.CreateQueueResult.(to_json (of_json (to_json resp)))));
+  let delete_queue queue_url region =
+    Runtime.(un_m (run_request
+                     ~region
+                     (module DeleteQueue)
+                     (Types.DeleteQueueRequest.make ~queue_url ())))
+
+  let create_queue queue_name region =
+    Runtime.(un_m (run_request
+                     ~region
+                     (module CreateQueue)
+                     (Types.CreateQueueRequest.make ~queue_name ())))
+
+  let arb_cooking =
+    let c = List.map (QCheck.Gen.pure) Aws_test.Corpus.cooking in
+    QCheck.make @@ QCheck.Gen.oneof c
+
+  let create_delete_queue_test =
+    QCheck.Test.make ~count:1
+    ~name:"SQS create / delete queue"
+    QCheck.(arb_cooking)
+    (fun queue_name ->
+      let create_res = create_queue queue_name test_region in
+
+      begin match create_res with
+         | `Ok resp ->
+            Printf.printf "%s\n" (Yojson.Basic.to_string (Types.CreateQueueResult.(to_json (of_json (to_json resp)))));
           true
        | `Error err -> begin Printf.printf "Error: %s\n" (Aws.Error.format Errors_internal.to_string err); false end
-       end;
-    let queue_url = match res with
-      | `Ok resp ->
-         resp.queue_url
-      | `Error err -> assert false
-    in
+      end
 
-    let delete_res = Runtime.(un_m (run_request
-                             ~region:"us-east-1"
-                             (module DeleteQueue)
-                             (Types.DeleteQueueRequest.make ~queue_url:(from_opt queue_url) ()))) in
-
-    "Delete Queue"
-    @? begin match delete_res with
-       | `Ok resp ->
-          true
-       | `Error err -> begin Printf.printf "Error: %s\n" (Aws.Error.format Errors_internal.to_string err); false end
-       end
-
+      (* let queue_url = match create_res with
+       *   | `Ok resp -> from_opt resp.queue_url
+       *   | `Error err -> assert false
+       * in
+       *
+       * let delete_res = delete_queue queue_url "us-east-1" in
+       * "Delete Queue"
+       * @? begin match delete_res with
+       *    | `Ok resp -> true
+       *    | `Error err -> begin Printf.printf "Error: %s\n" (Aws.Error.format Errors_internal.to_string err); false end
+       *    end *)
+    )
   let test_cases =
-    [ "SQS create / delete queue" >:: create_delete_queue_test ]
+    [ create_delete_queue_test ]
 
   let rec was_successful =
     function
@@ -61,13 +76,12 @@ module TestSuite(Runtime : sig
     | RTodo _::_ ->
       false
   let _ =
-    let suite = "Tests" >::: test_cases in
     let verbose = ref false in
     let set_verbose _ = verbose := true in
     Arg.parse
       [("-verbose", Arg.Unit set_verbose, "Run the test in verbose mode.");]
       (fun x -> raise (Arg.Bad ("Bad argument : " ^ x)))
       ("Usage: " ^ Sys.argv.(0) ^ " [-verbose]");
-    if not (was_successful (run_test_tt ~verbose:!verbose suite)) then
+    if not (was_successful (QCheck_runner.run_tests_main test_cases)) then
       exit 1
 end
