0796acbf0af371bf6bae7c1ba769d663a9b2a911
Tim McGilchrist <timmcgil@gmail.com>
2020-02-04 21:59:04 +1100

    Apply more arguments to run_request.

--- libraries/sqs/lib_test/test_lwt.ml
+++ libraries/sqs/lib_test/test_lwt.ml
@@ -7,6 +7,6 @@ module T = TestSuite(struct
     let secret_key = Unix.getenv "AWS_SECRET_KEY"
     let region = Unix.getenv "AWS_DEFAULT_REGION"
 
-    let run_request = Aws_lwt.Runtime.run_request ~access_key ~secret_key ~region
+    let run_request x = Aws_lwt.Runtime.run_request ~access_key ~secret_key ~region x
     let un_m = Lwt_main.run
   end)
